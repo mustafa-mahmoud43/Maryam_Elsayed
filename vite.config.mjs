56c05db5eeb1be39758966b39b448096c3ce5f65
mustafa-mahmoud203 <must.afa000262@gmail.com>
2025-11-14 19:32:13 +0200

    12

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/code/', // ضروري لموقعك على GitHub Pages
+  base: "/code/", // ضروري لموقعك على GitHub Pages
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-})
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
